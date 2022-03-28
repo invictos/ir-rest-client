0cb38986e4c211e675c2567307b6b9c85e58b472
Camusat Antoine <antoine.camusat@insa-rouen.fr>
2022-03-28 06:07:54 +0200

    readme

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# Client
+# IRRest Client
 
-## Compile & package
-```
+Ceci est le client pour le projet **fr.tvmp.irrest**
+
+
+## Package vers un .jar
+```bash
 mvn clean compile assembly:single
+```
+
+## Execution
+```bash
+#BasePath=http://127.0.0.1:8080/tvmp/api
+java -jar target/ir-rest-client-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+#On peut passer le basePath de l'API en argument
+java -jar target/ir-rest-client-1.0-SNAPSHOT-jar-with-dependencies.jar http://ipv4.ovh:2803/tvmp/api
 ```
--- src/main/java/fr/tvmp/irrest/client/CPAMApplication.java
+++ src/main/java/fr/tvmp/irrest/client/CPAMApplication.java
@@ -3,8 +3,8 @@ package fr.tvmp.irrest.client;
 import fr.tvmp.irrest.client.procedures.MainMenu;
 
 public class CPAMApplication {
-    //private static final String DEFAULT_API_URL = "http://127.0.0.1:8080/tvmp/api";
-    private static final String DEFAULT_API_URL = "http://ipv4.ovh:8080/tvmp/api";
+    private static final String DEFAULT_API_URL = "http://127.0.0.1:8080/tvmp/api";
+    //private static final String DEFAULT_API_URL = "http://ipv4.ovh:2803/tvmp/api";
     public static void main(String[] args) {
         loadContext(args);
 
@@ -14,7 +14,7 @@ public class CPAMApplication {
 
 
     private static void loadContext(String[] args) {
-        Context.create(args.length > 1 ? args[0] : DEFAULT_API_URL);
+        Context.create(args.length > 0 ? args[0] : DEFAULT_API_URL);
     }
 
 }
